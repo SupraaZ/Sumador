8f0873a6253a2bb5e001842223e4c588d3a22e8c
Adrian Granero <adriangran17@gmail.com>
2020-03-09 10:09:02 +0100

    Final

---
+++ Sumador/src/AGCFunciona.java
@@ -0,0 +1,17 @@
+
+public class AGCFunciona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ASumar sumi = new ASumar("3");
+		
+		
+		ASumar sumi2 = new ASumar("-4");
+		
+		ASumar sumi3 = new ASumar("453");
+		
+
+	}
+
+}
