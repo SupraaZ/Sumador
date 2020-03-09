6d35458aa527cd16d85273665457141a0e9d1e51
Adrian Granero <adriangran17@gmail.com>
2020-03-09 09:20:48 +0100

    Primera prueba.

--- Sumador/src/ASumar.java
+++ Sumador/src/ASumar.java
@@ -1,4 +1,29 @@
 
 public class ASumar {
 
+	private String valor_inicial;
+
+	/**
+	 * 
+	 * @param string 
+	 */
+	public ASumar(String string) {
+		// TODO Auto-generated constructor stub
+		this.valor_inicial = string;
+	}
+
+	/**
+	 * 
+	 * @return Devuelve el resultado
+	 */
+	public String mostrar() {
+		// TODO Auto-generated method stub
+		String numero = valor_inicial;
+        if (numero.length() == 1) {
+            numero = (numero + " = " + numero);
+        }
+        return numero;
+        
+	}
+
 }
--- Sumador/test/TestASumar.java
+++ Sumador/test/TestASumar.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 
 class TestASumar {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
+@Test
+public void testUnaCifra() {
+    ASumar sumi = new ASumar("5");
+    String resultado = sumi.mostrar();
+    assertEquals("5 = 5",resultado );
+    
+}
 }
